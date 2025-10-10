9796cd5c650704d4c5e88a78894c51d94e6917c8
Daniel Pessoa Cardeal <danielpessoa.cardeal@gmail.com>
2025-10-10 11:58:15 -0300

    Implement line-by-line tcp parser

--- main.go
+++ main.go
@@ -4,30 +4,59 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 	"strings"
 )
 
-func main() {
-	file, err := os.Open("./messages.txt")
-	if err != nil {
-		log.Fatal("Failed to open messages.txt")
-	}
+const SERVER_PORT = ":42069"
 
-	readBuff := make([]byte, 8)
-	currentLine := ""
-	n, err := file.Read(readBuff)
-	for err != io.EOF && n != 0 {
-		segments := strings.Split(string(readBuff[:n]), "\n")
-		currentLine += segments[0]
-		if len(segments) > 1 {
-			fmt.Printf("read: %s\n", currentLine)
-			var i int
-			for i = 1; i < len(segments)-1; i++ {
-				fmt.Printf("read: %s\n", segments[i])
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		defer close(c)
+		defer f.Close()
+
+		readBuff := make([]byte, 8)
+		currentLine := ""
+		n, err := f.Read(readBuff)
+		for err != io.EOF {
+			segments := strings.Split(string(readBuff[:n]), "\n")
+			currentLine += segments[0]
+			if len(segments) > 1 {
+				c <- currentLine
+				var i int
+				for i = 1; i < len(segments)-1; i++ {
+					c <- segments[i]
+				}
+				currentLine = segments[i]
 			}
-			currentLine = segments[i]
+			n, err = f.Read(readBuff)
 		}
-		n, err = file.Read(readBuff)
+	}()
+
+	return c
+}
+
+func main() {
+	ln, err := net.Listen("tcp", SERVER_PORT)
+	if err != nil {
+		log.Fatalf("Unable to listen on port %s: %s", SERVER_PORT, err)
+	}
+	defer ln.Close()
+
+	log.Printf("Server up on port %s", SERVER_PORT)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("Failed to accept connection: %s", err)
+			continue
+		}
+		log.Printf("Connection accepted from %s", conn.RemoteAddr())
+		c := getLinesChannel(conn)
+		for line := range c {
+			fmt.Println(line)
+		}
 	}
 }
